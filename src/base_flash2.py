fix mismatch in returned gradients

src/base_flash2.py
@@ -20,7 +20,7 @@ def _fwd_kernel(
     K,
     V,
     sm_scale,
-    # Mask,
+    Mask,
     L,
     Out,
     stride_qz,
@@ -35,6 +35,10 @@ def _fwd_kernel(
     stride_vh,
     stride_vk,
     stride_vn,
+    stride_maskz,
+    stride_maskh,
+    stride_maskm,
+    stride_maskn,
     stride_oz,
     stride_oh,
     stride_om,
@@ -83,7 +87,16 @@ def _fwd_kernel(
     l_i = tl.zeros([BLOCK_M], dtype=tl.float32)
     acc = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
     if HAS_MASK:
-        pass
+        off_z = off_hz // H
+        off_h = off_hz % H
+        Mask_block_ptr = tl.make_block_ptr(
+            base=Mask + off_z * stride_maskz + off_h * stride_maskh,
+            shape=(N_CTX, N_CTX),
+            strides=(stride_maskm, stride_maskn),
+            offsets=(start_m * BLOCK_M, 0),
+            block_shape=(BLOCK_M, BLOCK_N),
+            order=(1, 0),
+        )
 
     # credits to: Adam P. Goucher (https://github.com/apgoucher):
     # scale sm_scale by 1/log_2(e) and use
@@ -107,7 +120,9 @@ def _fwd_kernel(
             )
         qk += tl.dot(q, k, allow_tf32=True)
         if HAS_MASK:
-            pass
+            mask = tl.load(Mask_block_ptr)
+            qk += mask.to(qk.dtype)
+
         # -- compute scaling constant ---
         m_i_new = tl.maximum(m_i, tl.max(qk, 1))
         alpha = tl.math.exp2(m_i - m_i_new)
@@ -123,7 +138,7 @@ def _fwd_kernel(
         K_block_ptr = tl.advance(K_block_ptr, (0, BLOCK_N))
         V_block_ptr = tl.advance(V_block_ptr, (BLOCK_N, 0))
         if HAS_MASK:
-            pass
+            Mask_block_ptr = tl.advance(Mask_block_ptr, (0, BLOCK_N))
     # write back l and m
     acc = acc / l_i[:, None]
     l_ptrs = L + off_hz * N_CTX + offs_m
@@ -160,12 +175,32 @@ def _bwd_preprocess(
 
 
 @jit
+def _bwd_preprocess(
+    Out,
+    DO,
+    Delta,
+    BLOCK_M: tl.constexpr,
+    D_HEAD: tl.constexpr,
+):
+    off_m = tl.program_id(0) * BLOCK_M + tl.arange(0, BLOCK_M)
+    off_n = tl.arange(0, D_HEAD)
+    # load
+    o = tl.load(Out + off_m[:, None] * D_HEAD + off_n[None, :]).to(tl.float32)
+    do = tl.load(DO + off_m[:, None] * D_HEAD + off_n[None, :]).to(tl.float32)
+    # compute
+    delta = tl.sum(o * do, axis=1)
+    # write-back
+    tl.store(Delta + off_m, delta)
+
+
+@jit
 def _bwd_kernel_one_col_block(
     Q,
     K,
     V,
     sm_scale,
     qk_scale,
+    Mask,
     Out,
     DO,
     DQ,
@@ -186,6 +221,10 @@ def _bwd_kernel_one_col_block(
     stride_vh,
     stride_vk,
     stride_vn,
+    stride_maskz,
+    stride_maskh,
+    stride_maskm,
+    stride_maskn,
     Z,
     H,
     N_CTX,
@@ -215,7 +254,9 @@ def _bwd_kernel_one_col_block(
     k_ptrs = K + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk)
     v_ptrs = V + (offs_n[:, None] * stride_vk + offs_k[None, :] * stride_vn)
     if HAS_MASK:
-        pass
+        mask_ptrs = Mask + (
+            offs_qm[:, None] * stride_maskm + offs_n[None, :] * stride_maskn
+        )
     do_ptrs = DO + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
     dq_ptrs = DQ + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
     # pointer to row-wise quantities in value-like data
@@ -234,16 +275,13 @@ def _bwd_kernel_one_col_block(
         q = tl.load(q_ptrs)
         # recompute p = softmax(qk, dim=-1).T
         # NOTE: `do` is pre-divided by `l`; no normalization here
-        if CAUSAL:
-            qk = tl.where(
-                offs_m_curr[:, None] >= (offs_n[None, :]), float(0.0), float("-inf")
-            )
-        else:
-            qk = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
-        qk += tl.dot(q, tl.trans(k))
+        qk = tl.dot(q, tl.trans(k))
         qk *= qk_scale
         if HAS_MASK:
-            pass
+            mask = tl.load(mask_ptrs)
+            qk += mask.to(qk.dtype)
+        if CAUSAL:
+            qk = tl.where(offs_m_curr[:, None] >= (offs_n[None, :]), qk, float("-inf"))
         l_i = tl.load(l_ptrs + offs_m_curr)
         p = tl.math.exp2(qk - l_i[:, None])
         # compute dv
@@ -271,7 +309,137 @@ def _bwd_kernel_one_col_block(
         dq_ptrs += BLOCK_M * stride_qm
         q_ptrs += BLOCK_M * stride_qm
         if HAS_MASK:
-            pass
+            mask_ptrs += BLOCK_M * stride_maskm
+        do_ptrs += BLOCK_M * stride_qm
+    # write-back
+    dv_ptrs = DV + (offs_n[:, None] * stride_vk + offs_k[None, :] * stride_vn)
+    dk_ptrs = DK + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk)
+    tl.store(dv_ptrs, dv)
+    tl.store(dk_ptrs, dk)
+
+
+@jit
+def _bwd_kernel_one_col_block(
+    Q,
+    K,
+    V,
+    sm_scale,
+    qk_scale,
+    Mask,
+    Out,
+    DO,
+    DQ,
+    DK,
+    DV,
+    L,
+    D,
+    stride_dqa,
+    stride_qz,
+    stride_qh,
+    stride_qm,
+    stride_qk,
+    stride_kz,
+    stride_kh,
+    stride_kn,
+    stride_kk,
+    stride_vz,
+    stride_vh,
+    stride_vk,
+    stride_vn,
+    stride_maskz,
+    stride_maskh,
+    stride_maskm,
+    stride_maskn,
+    Z,
+    H,
+    N_CTX,
+    off_hz,
+    start_n,
+    num_block,
+    BLOCK_M: tl.constexpr,
+    BLOCK_DMODEL: tl.constexpr,
+    BLOCK_N: tl.constexpr,
+    SEQUENCE_PARALLEL: tl.constexpr,
+    CAUSAL: tl.constexpr,
+    HAS_MASK: tl.constexpr,
+):
+    if SEQUENCE_PARALLEL:
+        DQ += stride_dqa.to(tl.int64) * start_n
+    if CAUSAL:
+        lo = start_n * BLOCK_M
+    else:
+        lo = 0
+    # initialize row/col offsets
+    offs_qm = lo + tl.arange(0, BLOCK_M)
+    offs_n = start_n * BLOCK_M + tl.arange(0, BLOCK_M)
+    offs_m = tl.arange(0, BLOCK_N)
+    offs_k = tl.arange(0, BLOCK_DMODEL)
+    # initialize pointers to value-like data
+    q_ptrs = Q + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+    k_ptrs = K + (offs_n[:, None] * stride_kn + offs_k[None, :] * stride_kk)
+    v_ptrs = V + (offs_n[:, None] * stride_vk + offs_k[None, :] * stride_vn)
+    if HAS_MASK:
+        mask_ptrs = Mask + (
+            offs_qm[:, None] * stride_maskm + offs_n[None, :] * stride_maskn
+        )
+    do_ptrs = DO + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+    dq_ptrs = DQ + (offs_qm[:, None] * stride_qm + offs_k[None, :] * stride_qk)
+    # pointer to row-wise quantities in value-like data
+    D_ptrs = D + off_hz * N_CTX
+    l_ptrs = L + off_hz * N_CTX
+    # initialize dv amd dk
+    dv = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
+    dk = tl.zeros([BLOCK_M, BLOCK_DMODEL], dtype=tl.float32)
+    # k and v stay in SRAM throughout
+    k = tl.load(k_ptrs)
+    v = tl.load(v_ptrs)
+    # loop over rows
+    for start_m in range(lo, num_block * BLOCK_M, BLOCK_M):
+        offs_m_curr = start_m + offs_m
+        # load q, k, v, do on-chip
+        q = tl.load(q_ptrs)
+        # recompute p = softmax(qk, dim=-1).T
+        # NOTE: `do` is pre-divided by `l`; no normalization here
+        if CAUSAL:
+            qk = tl.where(
+                offs_m_curr[:, None] >= (offs_n[None, :]), float(0.0), float("-inf")
+            )
+        else:
+            qk = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32)
+        qk += tl.dot(q, tl.trans(k))
+        qk *= qk_scale
+        if HAS_MASK:
+            mask = tl.load(mask_ptrs)
+            qk += mask.to(qk.dtype)
+
+        l_i = tl.load(l_ptrs + offs_m_curr)
+        p = tl.math.exp2(qk - l_i[:, None])
+        # compute dv
+        do = tl.load(do_ptrs)
+        dv += tl.dot(tl.trans(p.to(Q.dtype.element_ty)), do, allow_tf32=True)
+        # compute dp = dot(v, do)
+        Di = tl.load(D_ptrs + offs_m_curr)
+        # dp = tl.zeros([BLOCK_M, BLOCK_N], dtype=tl.float32) - Di[:, None]
+        dp = tl.dot(do, tl.trans(v), allow_tf32=True)
+        # compute ds = p * (dp - delta[:, None])
+        ds = (p * (dp - Di[:, None]) * sm_scale).to(Q.dtype.element_ty)
+        # compute dk = dot(ds.T, q)
+        dk += tl.dot(tl.trans(ds), q, allow_tf32=True)
+        # compute dq
+        if not SEQUENCE_PARALLEL:
+            dq = tl.load(dq_ptrs)
+            dq += tl.dot(ds, k, allow_tf32=True)
+            tl.store(dq_ptrs, dq)
+        elif SEQUENCE_PARALLEL:
+            # dq = tl.dot(ds, k, allow_tf32=True)
+            dq = tl.trans(tl.dot(tl.trans(k), tl.trans(ds), allow_tf32=True))
+            tl.store(dq_ptrs, dq)
+
+        # increment pointers
+        dq_ptrs += BLOCK_M * stride_qm
+        q_ptrs += BLOCK_M * stride_qm
+        if HAS_MASK:
+            mask_ptrs += BLOCK_M * stride_maskm
         do_ptrs += BLOCK_M * stride_qm
     # write-back
     dv_ptrs = DV + (offs_n[:, None] * stride_vk + offs_k[None, :] * stride_vn)
@@ -284,6 +452,7 @@ def _bwd_kernel_one_col_block(
 def _bwd_kernel(
     # fmt: off
     Q, K, V, sm_scale,
+    Mask,
     Out, DO,
     DQ, DK, DV,
     L,
@@ -291,6 +460,7 @@ def _bwd_kernel(
     stride_dqa, stride_qz, stride_qh, stride_qm, stride_qk,
     stride_kz, stride_kh, stride_kn, stride_kk,
     stride_vz, stride_vh, stride_vk, stride_vn,
+    stride_maskz, stride_maskh, stride_maskm, stride_maskn,
     Z, H, N_CTX,
     BLOCK_M: tl.constexpr, BLOCK_DMODEL: tl.constexpr,
     BLOCK_N: tl.constexpr,
@@ -308,7 +478,7 @@ def _bwd_kernel(
     K += off_z * stride_kz + off_h * stride_kh
     V += off_z * stride_vz + off_h * stride_vh
     if HAS_MASK:
-        pass
+        Mask += off_z * stride_maskz + off_h * stride_maskh
     DO += off_z * stride_qz + off_h * stride_qh
     DQ += off_z * stride_qz + off_h * stride_qh
     DK += off_z * stride_kz + off_h * stride_kh
@@ -323,6 +493,7 @@ def _bwd_kernel(
                 V,
                 sm_scale,
                 qk_scale,
+                Mask,
                 Out,
                 DO,
                 DQ,
@@ -343,6 +514,10 @@ def _bwd_kernel(
                 stride_vh,
                 stride_vk,
                 stride_vn,
+                stride_maskz,
+                stride_maskh,
+                stride_maskm,
+                stride_maskn,
                 Z,
                 H,
                 N_CTX,
@@ -364,6 +539,7 @@ def _bwd_kernel(
             V,
             sm_scale,
             qk_scale,
+            Mask,
             Out,
             DO,
             DQ,
@@ -384,6 +560,10 @@ def _bwd_kernel(
             stride_vh,
             stride_vk,
             stride_vn,
+            stride_maskz,
+            stride_maskh,
+            stride_maskm,
+            stride_maskn,
             Z,
             H,
             N_CTX,
@@ -401,7 +581,16 @@ def _bwd_kernel(
 
 class _attention(torch.autograd.Function):
     @staticmethod
-    def forward(ctx, q, k, v, causal, sm_scale, mask=None, sequence_parallel=False):
+    def forward(
+        ctx,
+        q,
+        k,
+        v,
+        causal,
+        sm_scale,
+        mask: torch.Tensor = None,
+        sequence_parallel=False,
+    ):
         # only support for Ampere now
         capability = torch.cuda.get_device_capability()
         if capability[0] < 8:
@@ -420,15 +609,23 @@ class _attention(torch.autograd.Function):
             (q.shape[0] * q.shape[1], q.shape[2]), device=q.device, dtype=torch.float32
         )
         num_warps = 4 if Lk <= 64 else 8
+
         has_mask = mask is not None
         if has_mask:
-            pass
+            assert isinstance(mask, torch.Tensor) and 1 <= mask.dim() <= 4
+            extra_dims = 4 - mask.dim()
+            mask = mask.reshape((1,) * extra_dims + mask.shape)
+            mask = mask.expand(q.shape[0], q.shape[1], q.shape[2], k.shape[2])
+            mask_strides = mask.stride()
+        else:
+            mask_strides = (None,) * 4
+
         _fwd_kernel[grid](
             q,
             k,
             v,
             sm_scale,
-            # mask,
+            mask,
             L,
             o,
             q.stride(0),
@@ -443,6 +640,7 @@ class _attention(torch.autograd.Function):
             v.stride(1),
             v.stride(2),
             v.stride(3),
+            *mask_strides,
             o.stride(0),
             o.stride(1),
             o.stride(2),
@@ -459,7 +657,7 @@ class _attention(torch.autograd.Function):
             num_stages=4,
         )
 
-        ctx.save_for_backward(q, k, v, o, L)
+        ctx.save_for_backward(q, k, v, mask, o, L)
         ctx.grid = grid
         ctx.sm_scale = sm_scale
         ctx.BLOCK_DMODEL = Lk
@@ -470,7 +668,7 @@ class _attention(torch.autograd.Function):
     @staticmethod
     def backward(ctx, do):
         BLOCK = 128
-        q, k, v, o, L = ctx.saved_tensors
+        q, k, v, mask, o, L = ctx.saved_tensors
         sequence_parallel = ctx.sequence_parallel
         seq_len_kv = k.shape[2]
         do = do.contiguous()
@@ -483,9 +681,13 @@ class _attention(torch.autograd.Function):
         dk = torch.empty_like(k)
         dv = torch.empty_like(v)
         delta = torch.empty_like(L)
-        has_mask = None  # mask is not None
+
+        has_mask = mask is not None
         if has_mask:
-            pass
+            mask_strides = mask.stride()
+        else:
+            mask_strides = (None,) * 4
+
         _bwd_preprocess[(ctx.grid[0] * ctx.grid[1],)](
             o,
             do,
@@ -498,6 +700,7 @@ class _attention(torch.autograd.Function):
             k,
             v,
             ctx.sm_scale,
+            mask,
             o,
             do,
             dq,
@@ -518,6 +721,7 @@ class _attention(torch.autograd.Function):
             v.stride(1),
             v.stride(2),
             v.stride(3),
+            *mask_strides,
             q.shape[0],
             q.shape[1],
             q.shape[2],
@@ -533,7 +737,7 @@ class _attention(torch.autograd.Function):
 
         if len(dq.shape) == 5:
             dq = dq.sum(dim=0)
-        return dq, dk, dv, None, None, None
+        return dq, dk, dv, None, None, None, None
 
 
 attention = _attention.apply

src/test_flash2_alibi.py
@@ -7,6 +7,7 @@ import triton.ops
 
 from alibi_flashv2 import flash2_alibi
 from base_flash2 import attention as baseAttention
+from pr_alibi import attention as pr_Attention
 
 
 def _banded_sq_matrix(size, value=1, device=None, dtype=None):
@@ -30,7 +31,7 @@ masked = False
 )
 @pytest.mark.parametrize("dtype", [torch.bfloat16])
 @pytest.mark.parametrize("causal", [True])
-@pytest.mark.parametrize("masked", [False])
+@pytest.mark.parametrize("masked", [True, False])
 @pytest.mark.parametrize("seq_par", [False, True])
 def test_op(Z, H, N_CTX, D_HEAD, dtype, causal, masked, seq_par):
     capability = torch.cuda.get_device_capability()
@@ -93,7 +94,8 @@ def test_op(Z, H, N_CTX, D_HEAD, dtype, causal, masked, seq_par):
     # # triton implementation
     # tri_out = flash2_alibi(q, k, v, causal, sm_scale, M_triton, seq_par)
     # tri_out = triton.ops.attention(q, k, v, causal, sm_scale, seq_par)
-    tri_out = baseAttention(q, k, v, causal, sm_scale, seq_par)
+    tri_out = baseAttention(q, k, v, causal, sm_scale, M_triton, seq_par)
+    # tri_out = pr_Attention(q, k, v, causal, sm_scale, M_triton, seq_par)
     print(f"{ref_out.shape=}, {ref_out[0][0][0][0]=}")
     print(f" {tri_out.shape=}, {tri_out[0][0][0][0]=}")
     tri_out.backward(dout)