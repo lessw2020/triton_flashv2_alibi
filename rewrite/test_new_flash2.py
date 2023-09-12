50f0f3b1a135c2fc45908704e1d7b5b8b03b0cb5
lessw2020 <lessw@etrillium.com>
2023-09-12 23:06:53 +0000

    add make_qkv

--- rewrite/test_new_flash2.py
+++ rewrite/test_new_flash2.py
@@ -7,11 +7,23 @@ from new_flash2_alibi import new_flash2 as attention
 @pytest.mark.parametrize("batch, num_heads, seq_len, dim_head", [(1, 2, 64, 32, ),])
 @pytest.mark.parametrize("dtype",[torch.bfloat16])
 
+
 def test_attention(batch, num_heads, seq_len, dim_head, dtype):
     torch.manual_seed(2020)
 
-    q = torch.empty((batch, num_heads, seq_len, dim_head), dtype=dtype, device="cuda")
-    q.normal_(mean=0.0, std=0.5)
-    q.requires_grad_()
+    def make_qkv(batch, num_heads, seq_len, dim_head, dtype):
+        """produces consistent qkv's"""
+        res = torch.empty((batch, num_heads, seq_len, dim_head), dtype=dtype, device="cuda")
+        res.normal_(mean=0.0, std=0.5)
+        res.requires_grad_()
+        return res
+
+
+    q = make_qkv(batch, num_heads, seq_len, dim_head, dtype)
+    k = make_qkv(batch, num_heads, seq_len, dim_head, dtype)
+    v = make_qkv(batch, num_heads, seq_len, dim_head, dtype)
+
+    assert id(q) != id(k)  
+
 
     print(f"{q=}")
